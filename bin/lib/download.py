eb82471026313c86857905d76a9416542c3c3fad
ϻαđЋανα <madhava1908200@gmail.com>
2021-02-28 19:16:04 +0530

    add pla for download

--- bin/lib/download.py
+++ bin/lib/download.py
@@ -9,23 +9,29 @@ from threading import Thread
 from lib.core import RAND,NN
 from time import strftime as nall
 from lib.cwd import update,cwd
+from pyloadart import arrow
+
+TMP_VAL = 0
+TMP_VAL2 = 0
 
 def start(URL, Id):
+    global TMP_VAL2
     for i in range(len(URL)):
         if URL[-i] == "/":
             filename = URL[-i+1:]
             break
     try:
-        print(Element["DISPLY"]["STARTED"], Id)
         request = get(URL)
         if not request.text.startswith("--Read--\n"):
             with open(filename,"w") as Buffer:
                 Buffer.write(request.text)
             Buffer.close()
         else:
-            print("form -->",Id,RAND,request.text,NN);return
+            print("\rMSG form -->",Id, " "*48, "\n", RAND,request.text,NN);return
         request.close()
-        print(Element["DISPLY"]["COMPLET"], Id)
+        #print(Element["DISPLY"]["COMPLET"], Id)
+        TMP_VAL2 += 1
+        arrow(TMP_VAL2,  TMP_VAL, msg="Getting", end_with="()", color='r')
         """except OSError:
             print(Element["ERROR"]["SPACE"])"""
     except:
@@ -41,16 +47,21 @@ def Get(ID):
     return
 
 def Download(ID):
+    global TMP_VAL
     for Id in ID:
+
         try:
             if ","  in list(Id):
                 for i in Id.split(","):
                     Thread(target= Get , args=(int(i),)).start()
+                    TMP_VAL += 1
             elif "-" in list(Id):
                 for i in range([int(x) for x in Id.split("-")][0],[int(x) for x in Id.split("-")][1]+1):
                     Thread(target= Get , args=(int(i),)).start()
+                    TMP_VAL += 1
             else:
                 Thread(target= Get , args=(int(Id),)).start()
+                TMP_VAL += 1
         except ValueError:
             print(Element["ERROR"]["INT"], "-->" ,Id)
         except OSError:
