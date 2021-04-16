19926459879749ec3d20a50c71e5a4c1ef9c2da1
ϻαđЋανα <madhava1908200@gmail.com>
2021-04-16 13:52:06 +0530

    add aloas to zsh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 
 
 cwd(){
+    printf "\033[32m[+] New file created -> cwd.py\033[0m\n"
     echo "#!/bin/env python3
 from os import system
 cwd = '`pwd`'
@@ -13,24 +14,35 @@ def update(cwd=cwd):
 package(){
     which python3
     if [ $? -ne 0 ];then
-        echo "[!] python3 Not Installed"
+        printf "\033[31m[!] python3 Not Found\033[0m\n"
     fi
-    which pip3
+    which pip
     if [ $? -ne 0 ];then
-        echo "[!] python3-pip Not Installed"
+        printf "\033[31m[!] pip Not Found\nInstall it manually.\033[0m\n"
     fi
 }
 
 main(){
     cwd
     package
-    cat ~/.bashrc | grep -v li5tgen >> ./.tmp
-    cp ./.tmp ~/.bashrc
+    tmp1=$(mktemp)
+    tmp2=$(mktemp)
+    printf "\033[32m[+] creating backup ~/.bashrc.bac for ~/.bashrc\033[0m\n"
+    printf "\033[32m[+] creating backup ~/.zshrc.bac for ~/.zshrc\033[0m\n"
+    cp ~/.bashrc ~/.bashrc.bac 2>/dev/null
+    cp ~/.zshrc ~/.zshrc.bac 2>/dev/null
+    cat ~/.bashrc 2>/dev/null | grep -v li5tgen >> $tmp1
+    cp $tmp1 ~/.bashrc
+    cat ~/.zshrc 2>/dev/null| grep -v li5tgen >> $tmp2
+    cp $tmp2 ~/.zshrc
     echo "alias li5tgen=\"`pwd`/bin/li5tgen\"" >> ~/.bashrc
-    rm ./.tmp
-    python3 -m pip install -r req.txt
-    ./bin/li5tgen 2>/dev/null
-    mkdir ./bin/.db
-    exec bash
+    printf "\033[32m[+] alias add to .bashrc\033[0m\n"
+    echo "alias li5tgen=\"`pwd`/bin/li5tgen\"" >> ~/.zshrc
+    printf "\033[32m[+] alias add to .zshrc\033[0m\n"
+    printf "\033[32m[+] Installing requirement:\033[0m\n"
+    for i in $(cat req.txt);do printf "\033[36m[+] package:pip: $i\n\033[31m";done
+    python3 -m pip install -r req.txt 1>/dev/null
+    mkdir ./bin/.db 2>/dev/null
+    printf "\n\n\n\n\033[32m[*] alias appended to your shell configaration file.\n[*] open new terminal to trigger it.\n[*] run 'li5tgen help' in new terminal for more info.\033[0m\n"
 }
 main
